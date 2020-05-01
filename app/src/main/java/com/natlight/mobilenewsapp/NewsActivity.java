11d80701bbfc0e4c1e66e066ea155414017c8e80
Novykova Natalia <novykovanatalie@gmail.com>
2020-05-01 21:16:59 +0200

    Reformat code in News Activity.

--- app/src/main/java/com/natlight/mobilenewsapp/NewsActivity.java
+++ app/src/main/java/com/natlight/mobilenewsapp/NewsActivity.java
@@ -22,31 +22,30 @@ import com.natlight.mobilenewsapp.Model.News;
 import com.natlight.mobilenewsapp.adapter.NewsAdapter;
 import com.natlight.mobilenewsapp.services.NetworkService;
 import com.squareup.picasso.Picasso;
+
 import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class NewsActivity extends AppCompatActivity {
     public static final String API_KEY = "017ac0ad7b2c4367ba9e0a08343d8e9b";
-    final int DIALOG_EXIT = 1;
-    final String LOG_TAG = "My logs";
-    KenBurnsView kbv;
-    DiagonalLayout diagonalLayout;
-    NetworkService networkService;
+    KenBurnsView topImage;
     TextView topAuthor;
     TextView topTitle;
+    DiagonalLayout diagonalLayout;
+    NetworkService networkService;
     SwipeRefreshLayout swipeRefreshLayout;
     Context ctx;
     ProgressBar progressBar;
     Handler handler;
 
     String source = "";
-    String sortBy = "";
     String webHotURL = "";
 
     NewsAdapter adapter;
-    RecyclerView lstNews;
+    RecyclerView listArticles;
     RecyclerView.LayoutManager layoutManager;
 
     @Override
@@ -55,13 +54,13 @@ public class NewsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_news);
 
         /*Declare and initialize variables*/
-        diagonalLayout = findViewById(R.id.diagonalLayout);
+        diagonalLayout = findViewById(R.id.diagonal_layout);
         swipeRefreshLayout = findViewById(R.id.swipe_refresh_source);
-        progressBar = findViewById(R.id.SpinKitNews);
-        kbv = findViewById(R.id.top_image);
+        progressBar = findViewById(R.id.spin_kit_news);
+        topImage = findViewById(R.id.top_image);
         topAuthor = findViewById(R.id.top_author);
         topTitle = findViewById(R.id.top_title);
-        lstNews = findViewById(R.id.lstNews);
+        listArticles = findViewById(R.id.list_articles);
 
         ctx = this;
         networkService = NetworkService.getInstance();
@@ -83,8 +82,8 @@ public class NewsActivity extends AppCompatActivity {
                 startActivity(detail);
             }
         });
-        lstNews.setHasFixedSize(true);
-        lstNews.setLayoutManager(layoutManager);
+        listArticles.setHasFixedSize(true);
+        listArticles.setLayoutManager(layoutManager);
 
         if (getIntent() != null) {
             source = getIntent().getStringExtra("source");
@@ -97,41 +96,42 @@ public class NewsActivity extends AppCompatActivity {
     private void loadNews(String source, boolean isRefreshed) {
         //TODO: implement cache feature
         networkService.getNewsJSONApi()
-            .getNewestArticles(networkService.getAPIUrl(source, API_KEY))
-            .enqueue(new Callback<News>() {
-                @Override
-                public void onResponse(Call<News> call, Response<News> response) {
-                    progressBar.setVisibility(View.GONE);
-                    Picasso.get()
-                            .load(response.body().getArticles().get(0).getUrlToImage())
-                            .into(kbv);
-                    topTitle.setText(response.body().getArticles().get(0).getTitle());
-                    topAuthor.setText(response.body().getArticles().get(0).getAutor());
-                    webHotURL = response.body().getArticles().get(0).getUrl();
+                .getNewestArticles(networkService.getAPIUrl(source, API_KEY))
+                .enqueue(new Callback<News>() {
+                    @Override
+                    public void onResponse(Call<News> call, Response<News> response) {
+                        progressBar.setVisibility(View.GONE);
+                        Picasso.get()
+                                .load(response.body().getArticles().get(0).getUrlToImage())//TODO: article can have empty URL
+                                .into(topImage);
+                        topTitle.setText(response.body().getArticles().get(0).getTitle());
+                        topAuthor.setText(response.body().getArticles().get(0).getAutor());
+                        webHotURL = response.body().getArticles().get(0).getUrl();
 
-                    List<Article> removeFirstItem = response.body().getArticles();
-                    removeFirstItem.remove(0);
-                    adapter = new NewsAdapter(removeFirstItem, getBaseContext());
-                    adapter.notifyDataSetChanged();
-                    lstNews.setAdapter(adapter);
-                }
+                        List<Article> articleList = response.body().getArticles();
+                        /* We should delete first Article bwcause it show on Top, in separate View*/
+                        articleList.remove(0);
+                        adapter = new NewsAdapter(articleList, getBaseContext());
+                        adapter.notifyDataSetChanged();
+                        listArticles.setAdapter(adapter);
+                    }
 
-                @Override
-                public void onFailure(Call<News> call, Throwable t) {
-                    progressBar.setVisibility(View.GONE);
-                    Log.e("mobileNewsApp", String.format("Request failed. URL: %s ",
-                            networkService.getAPIUrl(source, API_KEY)));
+                    @Override
+                    public void onFailure(Call<News> call, Throwable t) {
+                        progressBar.setVisibility(View.GONE);
+                        Log.e("mobileNewsApp", String.format("Request failed. URL: %s ",
+                                networkService.getAPIUrl(source, API_KEY)));
 
-                    AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
-                    builder.setMessage("Request failed. please try again")
-                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int id) {
-                                    finish();
-                                }
-                            });
-                    builder.create()
-                            .show();
-                }
-            });
+                        AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
+                        builder.setMessage("Request failed. please try again")
+                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        finish();
+                                    }
+                                });
+                        builder.create()
+                                .show();
+                    }
+                });
     }
 }
