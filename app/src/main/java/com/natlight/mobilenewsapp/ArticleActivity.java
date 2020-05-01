b43bb9a3a42752e9c3e0171bebd306d2103b38f4
Novykova Natalia <novykovanatalie@gmail.com>
2020-05-01 21:20:35 +0200

    Refactor code in Article Activity.

--- app/src/main/java/com/natlight/mobilenewsapp/ArticleActivity.java
+++ app/src/main/java/com/natlight/mobilenewsapp/ArticleActivity.java
@@ -1,4 +1,5 @@
 package com.natlight.mobilenewsapp;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -6,18 +7,18 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
+
 public class ArticleActivity extends AppCompatActivity {
     WebView webView;
     ProgressBar progressBar;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.article_activity);
 
-        progressBar = findViewById(R.id.SpinKitArticle);
-        webView = findViewById(R.id.webView);
+        progressBar = findViewById(R.id.spin_kit_article);
+        webView = findViewById(R.id.web_view);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebChromeClient(new WebChromeClient());
         webView.setWebViewClient(new WebViewClient() {
@@ -30,6 +31,5 @@ public class ArticleActivity extends AppCompatActivity {
             if (!getIntent().getStringExtra("webURL").isEmpty())
                 webView.loadUrl(getIntent().getStringExtra("webURL"));
         }
-
     }
 }
